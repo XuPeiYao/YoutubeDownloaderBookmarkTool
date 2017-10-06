440cf3662a851c2222a771e6432b6364d68edd48
徐培堯 <st89382000@gmail.com>
2017-10-06 14:46:30 +0800

    實作影片擷取管線流程

---
+++ mainProcess.ts
@@ -0,0 +1,28 @@
+declare var swal;
+(async function(){
+	var extractor = new MediaGet.Extractors.YoutubeExtractor();
+	try{
+		var mediaInfos = await extractor.getMediaInfosAsync(location.href);
+		mediaInfos = mediaInfos.filter(function(y){
+			if(y.attributes.mime.indexOf("video") == -1)return true;
+			if(y.attributes.codecs.indexOf("+")> -1)return true;
+			return false;
+		});
+		var downloadList = mediaInfos.map(function(y){
+			return "<li><a download href=\"" + y.realUrl + "\">[" + y.attributes.mime.split("/")[1] + "]" + 
+			(y.attributes.size || ("Audio - " + y.attributes.bitrate )) + 
+			"</li>"
+		});
+		swal({
+			title: "Download",
+			text: "<ul>" + downloadList.join("") + "</ul>",
+			html: true
+		});
+	}catch(e){
+		swal({
+			title: e.name,
+			text: e.message,
+			type: "error"
+		});
+	}
+})();
