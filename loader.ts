c5f3124b92a0d6370fc7cfc14984f2a1ef47434a
徐培堯 <st89382000@gmail.com>
2017-10-06 14:52:44 +0800

    變更註解方式

--- loader.ts
+++ loader.ts
@@ -12,7 +12,7 @@ async function loadScript(src: string) : Promise<void>{
 var host = 'https://xupeiyao.github.io/YoutubeDownloaderBookmarkTool/';
 
 (async function(){
-    //非同步載入並等候SweetAlert、MediaGet.js
+    /*非同步載入並等候SweetAlert、MediaGet.js*/
     await Promise.all([
         loadScript('https://unpkg.com/sweetalert/dist/sweetalert.min.js'),
         loadScript(host + 'lib/mediaGet/mediaget.js')
